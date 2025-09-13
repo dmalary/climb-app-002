672b7071585abd25aff209255d45f9ae53930f45
madhat5 <davidmalary@gmail.com>
2025-09-13 14:29:19 -0400

    feat: build additional getters

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,3 +1,4 @@
+//UPDATE ALL WITH TRY CATCH ERR HANDLING
 export async function getUsers() {
   // const res = await fetch("/api/users"); 
   const res = await fetch("http://localhost:5000/api/users"); 
@@ -7,17 +8,18 @@ export async function getUsers() {
 }
 
 export async function getUserSessions(userId){
-  const res = await fetch(`http://localhost:5000/api/sessions/${userId}`);
+  const res = await fetch(`http://localhost:5000/api/user/sessions/${userId}`);
   return res.json();
 }
 
-// export async function getSessionClimbs(userId){
-export async function getSessionClimbs(userId){
-    const res = await fetch(`http://localhost:5000/api/sessions/${sessionID}/attempts/${userId}`);
+export async function getSessionClimbs(sessionID){
+    const res = await fetch(`http://localhost:5000/api/session/${sessionID}`);
   return res.json();
 }
 
 export async function getUserSends(userId){
+  const res = await fetch(`http://localhost:5000/api/sends/${userId}`);
+return res.json()
 }
 
 // export async function getHardestSend(userId){
