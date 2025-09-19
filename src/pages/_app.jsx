3496a6bc68b9312028da1452576338102694f2a0
madhat5 <davidmalary@gmail.com>
2025-09-19 17:50:15 -0400

    feat: add clerk auth

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,29 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+  return (
+    <ClerkProvider
+      {...pageProps}
+      appearance={{
+        cssLayerName: 'clerk',
+      }}
+    >
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
